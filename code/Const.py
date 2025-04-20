eb8e74e7f1e8e5ebd716a394dca87d6f3d0d1b7b
Cintia Dias <diaslindolfocintia@gmail.com>
2025-04-20 20:46:45 +0100

    Creating asset folder                           Inserting all assets                         Creating the menu: background, sound and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,15 @@
+# C - Constantes começadas pela letra C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M - Constantes começadas pela letra M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W - Constantes começadas pela letra W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
